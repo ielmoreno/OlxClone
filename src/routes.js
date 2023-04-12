c5b67a56e425e00226c9ef2b97756c2757afbc2b
ielmoreno <gabmormed@gmail.com>
2023-04-11 21:10:34 -0300

    Atualizando o sistema para a segunda e terceira aula

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,31 @@
 const express = require('express');
 const router = express.Router();
 
-const AuthController = require('./controllers/AuthController');
+const AuthValidator = require('./validators/AuthValidator');
+const UserValidator = require('./validators/UserValidator');
+const Auth = require('./middleware/Auth');
 
-router.get('/ping', (reg, res) =>{
-    res.json({pong : true});
+const AuthController = require('./controllers/AuthController');
+const UserController = require('./controllers/UserController');
+const AdsController = require('./controllers/AdsController');
+
+
+router.get('/ping', (req, res) => {
+	res.json({ pong: true });
 });
 
 router.post('/user/signin', AuthController.signin);
-router.post('/user/signup', AuthController.signup);
+router.post('/user/signup', AuthValidator.signup, AuthController.signup);
+
+router.get('/states', UserController.getStates);
+router.get('/categories', AdsController.getCategories);
+
+router.post('/ad/add', Auth.private, AdsController.addAction);
+router.get('/ad/list', AdsController.getList);
+router.get('/ad/item', AdsController.getItem);
+router.put('/ad/:id', Auth.private, AdsController.editAction);
+
+router.get('/user/me', Auth.private, UserController.info);
+router.put('/user/me', UserValidator.editAction, Auth.private, UserController.editAction);
 
 module.exports = router;
