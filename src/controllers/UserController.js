a2cfa21352e634291f3570e1ac4b8a621f70d8dd
ielmoreno <gabmormed@gmail.com>
2023-04-11 22:12:13 -0300

    Criando o UserController

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -33,5 +33,55 @@ module.exports = {
                 category: cat.slug
             });
         }
+        res.json({
+            name: user.name,
+            email: user.email,
+            state: state.name,
+            ad: adList
+        });
+    },
+    editAction: async (req, res) =>{
+        const erros = validationResult(req);
+        if(!erros.isEmpty()) {
+            res.json({ error: erros.mapped() });
+            return;
+        }
+        const data = matchedData(req);
+        let updates = {};
+        if(data.name){
+            updates.name = date.name;
+        }
+        if(data.email){
+            const emailCheck = await User.findOne({ email: data.email});
+            if(emailCheck){
+                res.json({error: 'E-mail já existe'});
+                return;
+            }
+            updates.email = data.email;
+
+        }
+        if(data.state){
+            if(mongoose.Types.ObjectId.isValid(data.state)) {
+                const stateItem = await State.findById(data.state);
+                if(!stateItem) {
+                    res.json({
+                        error: { state: {msg: 'Estado não existe'}}
+                    });
+                    return;
+                }
+                updates.state = data.state;
+                
+            } else {
+                res.json({
+                    error: { state: { msg: 'Código de estado inválido'}}
+                });
+                return;
+            }
+        }
+        if(data.password){
+            updates.passwordHash = await bcrypt.hash(data.password, 10);
+        }
+        await User.finderOneAndUpdate({ token: data.token }, { $set: updates });
+        res.json({ update: 'ok'});
     }
 }
